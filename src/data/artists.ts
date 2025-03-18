d449697535d4cff59f543beb186564f2e3414f17
Himitris <antoine.gautier83@gmail.com>
2025-03-18 17:36:08 +0100

    oui

--- src/data/artists.ts
+++ src/data/artists.ts
@@ -27,6 +27,24 @@ export const artists: Artist[] = [
     socialLinks: {}
   },
   {
+    id: "melanie",
+    name: "MELANIE LESAGE",
+    image: "../assets/artists/melanie.jpg",
+    description: "Mélanie Lesage vous embarque sur un voilier le long des côtes françaises et vous invite dans l’intimité de sa cabine... Réchauffées par le timbre solaire de Mélanie, les chansons de ce jeune trio sont simples et légères, accompagnées par les notes d’un Rhodes, d’un Lapsteel et d’une Contrebasse. Laissez-vous happer par cet univers poétique et plongez la tête la première dans ce cocon Pop-Folk pour un moment hors du temps ! Vous y croiserez probablement Pomme, First Aid Kit ou Norah Jones.",
+    socialLinks: {
+      instagram: "https://instagram.com/forro_dalua"
+    }
+  },
+  {
+    id: "cie-del-caravaggio",
+    name: "CIE DEL CARAVAGGIO",
+    image: "../assets/artists/cie.jpg",
+    description: "« Abel » est le making of d'une pièce de théâtre, où ce qui est joué masque la véritable histoire. La comédienne, la seule à rester, déçue par le rôle attribué et les promesses non tenues, veut abandonner le projet. Le metteur en scène va tout mettre en œuvre pour ne pas la laisser partir. Entre débats et répétitions, il cherche la bonne fin, mais chaque scène est le déclencheur des nouvelles histoires plein des possibilités et problèmes. Seront-ils combien sur scène au salut final ?",
+    socialLinks: {
+      instagram: "https://instagram.com/forro_dalua"
+    }
+  },
+  {
     id: "forro-da-lua",
     name: "Forró Da Lua",
     image: "../assets/artists/forrodalua.png",
@@ -35,4 +53,52 @@ export const artists: Artist[] = [
       instagram: "https://instagram.com/forro_dalua"
     }
   },
+];
+
+export const dj: Artist[] = [
+  {
+    id: "bonnie-spacey",
+    name: "Bonnie Spacey",
+    image: "../assets/artists/bonniespacey.png",
+    description: "Description de Bonnie Spacey.",
+    socialLinks: {
+      instagram: "https://instagram.com/bonnie_spacey"
+    }
+  },
+  {
+    id: "dame-fleuraux",
+    name: "Dame Fleuraux",
+    image: "../assets/artists/damefleuraux.png",
+    description: "Description de Dame Fleuraux.",
+    socialLinks: {
+      instagram: "https://instagram.com/dame_fleuraux"
+    }
+  },
+  {
+    id: "elona",
+    name: "Elona",
+    image: "../assets/artists/elona.png",
+    description: "Description d'Elona.",
+    socialLinks: {
+      instagram: "https://instagram.com/elona_dj"
+    }
+  },
+  {
+    id: "khey-mysterio",
+    name: "Khey Mysterio",
+    image: "../assets/artists/kheymysterio.png",
+    description: "Description de Khey Mysterio.",
+    socialLinks: {
+      instagram: "https://instagram.com/khey_mysterio"
+    }
+  },
+  {
+    id: "marie-prude",
+    name: "Marie Prude",
+    image: "../assets/artists/marieprude.png",
+    description: "Description de Marie Prude.",
+    socialLinks: {
+      instagram: "https://instagram.com/marie_prude"
+    }
+  }
 ];
